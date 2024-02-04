97359e9cd794e73918e6072c2c1222688b1f338d
Rimjhim Gupta <rimjhimgupta786@gmail.com>
2024-02-04 16:20:19 +0530

    add all files

---
+++ js/data.js
@@ -0,0 +1,27 @@
+const checkboxdata = [
+      {
+            planName: "Silver Plan",
+            companyName : "Tata",
+            totalPrice: 55000
+      },
+      {
+            planName: "Silver Plan",
+            companyName : "Tata",
+            totalPrice: 55000
+      },
+      {
+            planName: "Silver Plan",
+            companyName : "Tata",
+            totalPrice: 55000
+      },
+      {
+            planName: "Silver Plan",
+            companyName : "Tata",
+            totalPrice: 55000
+      },
+      {
+            planName: "Silver Plan",
+            companyName : "Tata",
+            totalPrice: 55000
+      }
+]
